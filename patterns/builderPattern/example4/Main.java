5c18a471c595e487e0cf5956ba5f37576a0b7c73
himel <2005107@cse.buet.ac.bd>
2024-02-03 15:57:59 +0600

    commit

--- patterns/builderPattern/example4/Main.java
+++ patterns/builderPattern/example4/Main.java
@@ -1,8 +1,7 @@
 package patterns.builderPattern.example4;
 
 public class Main
-{
-    public static void main(String[] args) {
+{ public static void main(String[] args) {
         OrderSystem system=new OrderSystem();
         system.takeOrder();
     }
